a9c1c46537fd943669bd262e592c6956f5bd0df6
polymorpher <aaron@qokka.ai>
2024-08-12 13:02:51 -0700

    fix compile issue; add env example

--- deploy/DeployPool.s.sol
+++ deploy/DeployPool.s.sol
@@ -2,6 +2,7 @@
 pragma solidity =0.8.18;
 
 import {PanopticFactory} from "@contracts/PanopticFactory.sol";
+import {PanopticPool} from "@contracts/PanopticPool.sol";
 
 import "forge-std/Script.sol";
 import "forge-std/console.sol";
@@ -9,16 +10,17 @@ import "forge-std/console.sol";
 contract DeployPool is Script {
     function run() public {
         uint256 DEPLOYER_PRIVATE_KEY = vm.envUint("DEPLOYER_PRIVATE_KEY");
-        uint256 FACTORY_ADDRESS = vm.envAddress("PANOPTIC_FACTORY_ADDRESS");
-        uint256 TOKEN0_ADDRESS = vm.envAddress("TOKEN0_ADDRESS");
-        uint256 TOKEN1_ADDRESS = vm.envUint("TOKEN1_ADDRESS");
-        uint256 FEE = vm.envUint("FEE");
-        uint256 SALT = vm.envUint("SALT");
+        address FACTORY_ADDRESS = vm.envAddress("PANOPTIC_FACTORY_ADDRESS");
+        address TOKEN0_ADDRESS = vm.envAddress("TOKEN0_ADDRESS");
+        address TOKEN1_ADDRESS = vm.envAddress("TOKEN1_ADDRESS");
+        uint24 FEE = uint24(vm.envUint("FEE"));
+        uint96 SALT = uint96(vm.envUint("SALT"));
         vm.startBroadcast(DEPLOYER_PRIVATE_KEY);
         PanopticFactory factory = PanopticFactory(FACTORY_ADDRESS);
-        console.log("Deploying new Panoptic pool... Token0=%s, token1=%s, fee=%s, salt=%s", TOKEN0_ADDRESS, TOKEN1_ADDRESS,uint24(FEE), uint96(SALT));
-        PanopticPool pool = factory.deployNewPool(TOKEN0_ADDRESS, TOKEN1_ADDRESS, uint24(FEE), uint96(SALT));
-        console.log("Deployed pool to %s (token0=%s, token1=%s, fee=%s, salt=%s)", pool)
+        console.log("Deploying new Panoptic pool... Token0=%s, token1=%s, fee=%s, salt=%s", TOKEN0_ADDRESS, TOKEN1_ADDRESS);
+        console.log("  - fee=%s, salt=%s", FEE, SALT);
+        PanopticPool pool = factory.deployNewPool(TOKEN0_ADDRESS, TOKEN1_ADDRESS, FEE, SALT);
+        console.log("Deployed pool to %s", pool);
 
     }
 }
