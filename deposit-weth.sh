5199f66d89a85b6957545117f5c83dbf438cf68d
polymorpher <aaron@qokka.ai>
2024-08-22 19:55:36 -0700

    add missing rpc for deposit weth

--- deposit-weth.sh
+++ deposit-weth.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 export $(grep -v '^#' .env.deposit-weth | xargs)
 
-cast send ${WETH_ADDRESS} "deposit()" --value ${ETH_AMOUNT} --from ${DEPLOYER} --private-key ${DEPLOYER_PRIVATE_KEY}
+cast send ${WETH_ADDRESS} "deposit()" --value ${ETH_AMOUNT} --from ${DEPLOYER} --private-key ${DEPLOYER_PRIVATE_KEY} --rpc-url http://127.0.0.1:8545
