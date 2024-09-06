8eed6010038115bf4e839a1dea7148bfdf026541
polymorpher <aaron@qokka.ai>
2024-09-06 08:41:22 -0700

    print balance before deploying pool

--- deploy/DeployPool.s.sol
+++ deploy/DeployPool.s.sol
@@ -26,8 +26,8 @@ contract DeployPool is Script {
         // first, approve factory to use both tokens, so it can pay the tokens to the pool (during position minting) on behalf of the deployer
         uint256 token0Balance = IERC20Partial(TOKEN0_ADDRESS).balanceOf(deployer);
         uint256 token1Balance = IERC20Partial(TOKEN1_ADDRESS).balanceOf(deployer);
-        uint256 token0Amount = WETH_ADDRESS == TOKEN0_ADDRESS ? 5e18 : 1000e6;
-        uint256 token1Amount = WETH_ADDRESS == TOKEN1_ADDRESS ? 5e18 : 1000e6;
+        uint256 token0Amount = WETH_ADDRESS == TOKEN0_ADDRESS ? 5e18 : 2000e6;
+        uint256 token1Amount = WETH_ADDRESS == TOKEN1_ADDRESS ? 5e18 : 2000e6;
         if (token0Balance < token0Amount) {
             console.log("ERROR: Required %s token0, got %s", token0Amount, token0Balance);
             return;
@@ -39,7 +39,9 @@ contract DeployPool is Script {
         IERC20Partial(TOKEN0_ADDRESS).approve(address(factory), token0Amount);
         IERC20Partial(TOKEN1_ADDRESS).approve(address(factory), token1Amount);
 
-        console.log("Deploying new Panoptic pool... Token0=%s, token1=%s, fee=%s, salt=%s", TOKEN0_ADDRESS, TOKEN1_ADDRESS);
+        console.log("Token0 balance %s", token0Balance);
+        console.log("Token1 balance %s", token1Balance);
+        console.log("Deploying new Panoptic pool... Token0=%s, token1=%s", TOKEN0_ADDRESS, TOKEN1_ADDRESS);
         console.log("  - fee=%s, salt=%s", FEE, SALT);
         PanopticPool pool = factory.deployNewPool(TOKEN0_ADDRESS, TOKEN1_ADDRESS, FEE, SALT);
         console.log("Deployed pool to %s", address(pool));
